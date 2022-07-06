eb3a41639f91a690f1466b34b10c674c387dfabf
Danielle Sweeny <dsweeny1@gmail.com>
2022-07-06 17:32:06 -0600

    Add card and turn tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -9,39 +9,39 @@ describe('Turn', function() {
     let turn, card, card2;
 
     beforeEach(() => {
-      const card2 = new Card(
+      card2 = new Card(
         2,
         "What type of prototype method directly modifies the existing array?",
         ["mutator method", "accessor method", "iteration method"],
         "mutator method"
       );
-      const turn = new Turn();
-      const card = new Card();
+        turn = new Turn('iterator method', card2);
+        card = new Card();
     });
     
     it('should be a function', function(){
-        const turn = new Turn();
+     
         expect(Turn).to.be.a('function');
     })
 
     it('should be an instance of Turn', function() {
-        const turn = new Turn();
+       
         expect(turn).to.be.an.instanceof(Turn);
     })
 
     it('should be able to return guess', function() {
-        const turn = new Turn('user choice');
-        expect(turn.userGuess).to.equal('user choice')
+        
+        expect(turn.userGuess).to.equal('iterator method')
     })
 
     it('should be able to return card', function() {
-        const turn = new Turn('card choice', card2);
+       
         expect(turn.card).to.equal(card2)
     })
 
-    it('should be able to evaluate guess', function() {
-        const turn = new Turn(card2);
-        expect(turn.userGuess).to.equal(card2)
-        expect(turn.userGuess).to.equal(card2)
+    it('should be able to evaluate guess and give feedback', function() {
+       
+        expect(turn.evaluateGuess()).to.equal(false)
+        expect(turn.giveFeedback()).to.equal('Nope!')
     })
 })
