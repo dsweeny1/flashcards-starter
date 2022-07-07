8ec5b53f6d821858c0442f03ca10c6adee85fcc3
Danielle Sweeny <dsweeny1@gmail.com>
2022-07-06 19:40:11 -0600

    Correct previous commit

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -6,42 +6,42 @@ const Turn = require('../src/Turn');
 
 describe('Turn', function() {
 
-    let turn, card, card2;
+    let turn, card, card3;
 
     beforeEach(() => {
-      const card2 = new Card(
+      card3 = new Card(
         2,
         "What type of prototype method directly modifies the existing array?",
         ["mutator method", "accessor method", "iteration method"],
         "mutator method"
       );
-      const turn = new Turn();
-      const card = new Card();
+        turn = new Turn('mutator method', card3);
+        card = new Card();
     });
     
     it('should be a function', function(){
-        const turn = new Turn();
+       
         expect(Turn).to.be.a('function');
     })
 
     it('should be an instance of Turn', function() {
-        const turn = new Turn();
+        
         expect(turn).to.be.an.instanceof(Turn);
     })
 
     it('should be able to return guess', function() {
-        const turn = new Turn('user choice');
-        expect(turn.userGuess).to.equal('user choice')
+        
+        expect(turn.userGuess).to.equal('mutator method')
     })
 
     it('should be able to return card', function() {
-        const turn = new Turn('card choice', card2);
-        expect(turn.card).to.equal(card2)
+       
+        expect(turn.card).to.equal(card3)
     })
 
     it('should be able to evaluate guess', function() {
-        const turn = new Turn(card2);
-        expect(turn.userGuess).to.equal(card2)
-        expect(turn.userGuess).to.equal(card2)
+        
+        expect(turn.evaluateGuess()).to.equal(true)
+        expect(turn.giveFeedback()).to.equal('Correct!')
     })
 })
